3648ef039c12342c8554f1dd2ab0bf46d6acb25d
imalicx <xenrecords@yahoo.com>
2023-05-02 12:37:47 -0400

    exercise 1

---
+++ functions/simple_functions.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// favorite things
+
+function favoritethings(){
+let favoriteMovie = "Transformers2007";
+let favoriteBand = "Evanescence";
+let Myname = "Malic";
+
+console.log("My name is " + Myname + " and my favorite band is " + favoriteBand + ", and my favorite movie is " + favoriteMovie + "!");
+}
+
+// why im here
+
+function qhyimhere(){
+let reason1 = "I joined YearUp because I want to start a career early"
+let reason2 = "and test myself in a field I am most unfamiliar with"
+
+console.log(reason1 + " " + reason2 + "!");
+}
+//favorite places
+
+function favoriteplace(){
+let favoriteplace = "Orlando";
+let favoriteplacewhy = "it isn't that far, the area is so newly renovated, amusement parks, etc";
+
+console.log("My favorite place at the moment is " + favoriteplace + " because " + favoriteplacewhy + "!");
+}
+
+console.log("About me");
+favoritethings();
+
+console.log("Why Im here");
+qhyimhere();
+
+console.log("Favorite Place ATM");
+favoriteplace();
---
+++ lecturedemos/1-4.js
@@ -0,0 +1,8 @@
+// This is the function
+function displayGreeting() {
+let message = "Hello world!";
+console.log(message);
+}
+// elsewhere...
+// This is a call to the function
+displayGreeting();
