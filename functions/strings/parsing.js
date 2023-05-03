64f04fd3ee398c8aec4f4aa1354df166687ded66
imalicx <xenrecords@yahoo.com>
2023-05-03 13:37:17 -0400

    new exercises

---
+++ functions/strings/parsing.js
@@ -0,0 +1,27 @@
+"use script";
+
+//names
+
+let fullName = "Tony Stark";
+let nameArray = fullName.split(" ");
+let firstName = nameArray[0];
+let lastName = nameArray[1];
+
+console.log("Name: " + fullName);
+console.log("First name: " + firstName);
+console.log("Last name: " + lastName);
+
+// parse ruse
+
+function parseAndDisplayName(fullName) {
+    let spaceIndex = fullName.indexOf(" ");
+    let firstName = fullName.slice(0, spaceIndex);
+    let lastName = fullName.slice(spaceIndex + 1);
+    console.log("First name: " + firstName);
+    console.log("Last name: " + lastName);
+  }
+  
+  // Example usage:
+  parseAndDisplayName("Stephen Strange");
+  parseAndDisplayName("Bruce Banner");
+  
